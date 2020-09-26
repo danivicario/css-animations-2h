c76b8ebe9aa2be8aed64f3ca96c8dce40b7f3447
Dani Vicario <danivicario@dvicario.es>
2020-09-26 13:59:08 +0200

    added

--- examples/js/butterflies.js
+++ examples/js/butterflies.js
@@ -2,21 +2,21 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-Array(160)
+Array(24)
     .fill()
-    .forEach((i, idx) => {
-        let divDOMEl = document.createElement("div");
+    .forEach(() => {
+        const divDOMEl = document.createElement("div");
         divDOMEl.classList.add("butterfly");
         divDOMEl.style.background = `url(img/butterfly${randomInt(1, 6)}.png) no-repeat`;
         divDOMEl.style.backgroundSize = `cover`;
         divDOMEl.style.transform = `rotate3d(1, 1, 1, ${randomInt(-50, 50)}deg`;
 
-        let transitionTime = randomInt(500, 1500);
+        const transitionTime = randomInt(500, 1500);
         divDOMEl.style.transition = `all ${transitionTime / 1000}s`;
 
         setInterval(() => {
             divDOMEl.style.transform = `rotate3d(1, 1, 1, ${randomInt(-70, 70)}deg`;
         }, transitionTime);
 
-        document.body.appendChild(divDOMEl);
+        document.querySelector("#container").appendChild(divDOMEl);
     });
